f4383275c823dde40d1bfecded535765f198479a
Naventwo <tezikov07@inbox.ru>
2026-02-02 10:25:38 +0300

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,9 +1,15 @@
-iso_name="Arch-Linux-ISO-PE"
-iso_publisher=""
-iso_application="Arch Linux Live/Installer"
-iso_version="2024.01.01"
+iso_name="Arch-Naventwo"
+iso_publisher="Naventwo <https://github.com>"
+iso_application="Arch Linux Custom Live"
+iso_version="2024.01"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-ia32.grub.esp' 'uefi-x64.grub.esp' 'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
-pacman_conf="pacman.conf" # Можно скопировать стандартный из /etc/pacman.conf
+pacman_conf="pacman.conf"
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/usr/local/bin/install-desktop"]="0:0:755"
+  ["/usr/local/bin/install-server"]="0:0:755"
+)
